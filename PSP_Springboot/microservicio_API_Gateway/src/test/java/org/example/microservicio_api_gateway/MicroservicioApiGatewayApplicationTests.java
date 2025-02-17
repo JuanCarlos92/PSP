7c31dd577ccf86bf59125b0df1dc7da12677f83d
Juan Carlos <juancarlosfilter@gmail.com>
2025-02-17 10:40:11 +0100

    api gateway

---
+++ PSP_Springboot/microservicio_API_Gateway/src/test/java/org/example/microservicio_api_gateway/MicroservicioApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.microservicio_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
